d23d728f7c92353a32eeec37823051d661598c26
patarchims <patarchims@gmail.com>
2019-08-16 12:05:49 +0700

    Menambahkan laporan baru

---
+++ Report/frmKartuKontrol.Designer.vb
@@ -0,0 +1,158 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmKartuKontrol
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Panel3 = New System.Windows.Forms.Panel()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.CRV = New CrystalDecisions.Windows.Forms.CrystalReportViewer()
+        Me.RptKartu1 = New Aplikasi_Izin_Angkutan.RptKartu()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.BtnCetak = New System.Windows.Forms.Button()
+        Me.Label13 = New System.Windows.Forms.Label()
+        Me.txtnoKartu = New System.Windows.Forms.TextBox()
+        Me.Panel3.SuspendLayout()
+        Me.Panel1.SuspendLayout()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Panel3
+        '
+        Me.Panel3.BackColor = System.Drawing.Color.FromArgb(CType(CType(58, Byte), Integer), CType(CType(87, Byte), Integer), CType(CType(89, Byte), Integer))
+        Me.Panel3.Controls.Add(Me.Label2)
+        Me.Panel3.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Panel3.Location = New System.Drawing.Point(0, 0)
+        Me.Panel3.Name = "Panel3"
+        Me.Panel3.Size = New System.Drawing.Size(979, 48)
+        Me.Panel3.TabIndex = 74
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.Color.Transparent
+        Me.Label2.Font = New System.Drawing.Font("Calibri", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.White
+        Me.Label2.Location = New System.Drawing.Point(305, 4)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(300, 39)
+        Me.Label2.TabIndex = 70
+        Me.Label2.Text = "Laporan Kartu Kontrol"
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.CRV)
+        Me.Panel1.Location = New System.Drawing.Point(0, 106)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(979, 475)
+        Me.Panel1.TabIndex = 75
+        '
+        'CRV
+        '
+        Me.CRV.ActiveViewIndex = 0
+        Me.CRV.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.CRV.Cursor = System.Windows.Forms.Cursors.Default
+        Me.CRV.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.CRV.Location = New System.Drawing.Point(0, 0)
+        Me.CRV.Name = "CRV"
+        Me.CRV.ReportSource = Me.RptKartu1
+        Me.CRV.Size = New System.Drawing.Size(979, 475)
+        Me.CRV.TabIndex = 30
+        Me.CRV.ToolPanelView = CrystalDecisions.Windows.Forms.ToolPanelViewType.None
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.BackColor = System.Drawing.Color.Transparent
+        Me.GroupBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.GroupBox1.Controls.Add(Me.txtnoKartu)
+        Me.GroupBox1.Controls.Add(Me.BtnCetak)
+        Me.GroupBox1.Controls.Add(Me.Label13)
+        Me.GroupBox1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.GroupBox1.Font = New System.Drawing.Font("Tahoma", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox1.Location = New System.Drawing.Point(0, 48)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(979, 93)
+        Me.GroupBox1.TabIndex = 76
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "FILTER"
+        '
+        'BtnCetak
+        '
+        Me.BtnCetak.BackColor = System.Drawing.Color.Khaki
+        Me.BtnCetak.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.BtnCetak.Location = New System.Drawing.Point(511, 16)
+        Me.BtnCetak.Name = "BtnCetak"
+        Me.BtnCetak.Size = New System.Drawing.Size(139, 30)
+        Me.BtnCetak.TabIndex = 51
+        Me.BtnCetak.Text = "CETAK"
+        Me.BtnCetak.UseVisualStyleBackColor = False
+        '
+        'Label13
+        '
+        Me.Label13.AutoSize = True
+        Me.Label13.BackColor = System.Drawing.Color.Transparent
+        Me.Label13.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label13.Location = New System.Drawing.Point(11, 26)
+        Me.Label13.Name = "Label13"
+        Me.Label13.Size = New System.Drawing.Size(77, 13)
+        Me.Label13.TabIndex = 47
+        Me.Label13.Text = "Nomor Kartu"
+        '
+        'txtnoKartu
+        '
+        Me.txtnoKartu.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.txtnoKartu.Font = New System.Drawing.Font("Calibri", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtnoKartu.ForeColor = System.Drawing.Color.Black
+        Me.txtnoKartu.Location = New System.Drawing.Point(106, 20)
+        Me.txtnoKartu.Name = "txtnoKartu"
+        Me.txtnoKartu.Size = New System.Drawing.Size(328, 26)
+        Me.txtnoKartu.TabIndex = 52
+        '
+        'frmKartuKontrol
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(979, 584)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.Panel3)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "frmKartuKontrol"
+        Me.Text = "frmKartuKontrol"
+        Me.Panel3.ResumeLayout(False)
+        Me.Panel3.PerformLayout()
+        Me.Panel1.ResumeLayout(False)
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Panel3 As System.Windows.Forms.Panel
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents CRV As CrystalDecisions.Windows.Forms.CrystalReportViewer
+    Friend WithEvents RptKartu1 As Aplikasi_Izin_Angkutan.RptKartu
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents BtnCetak As System.Windows.Forms.Button
+    Friend WithEvents Label13 As System.Windows.Forms.Label
+    Friend WithEvents txtnoKartu As System.Windows.Forms.TextBox
+End Class
